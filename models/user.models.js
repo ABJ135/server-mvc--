12d3f3c71e819e3fc389ff1cc263de10afbf2505
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-23 14:31:01 +0500

    relation created

--- models/user.models.js
+++ models/user.models.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose')
 const {Schema} = require ('mongoose')
 
 const userSchema = new Schema ({
+
+    bookId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Book',
+        require:false
+    },
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
