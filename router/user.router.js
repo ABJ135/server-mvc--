25981e3bdd5cce60991701d9d3a555457374e783
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-23 14:06:20 +0500

    router completed

--- router/user.router.js
+++ router/user.router.js
@@ -3,3 +3,14 @@ const roouter = express.Router()
 
 const controller = require('../controller/user.controller')
 
+router.post('/createUser',controller.createUser)
+
+router.get('/getAllUsers',controller.getAllUsers)
+
+router.get('/getUserById',controller.getUserById)
+
+router.put('/updateUser',controller.updateUser)
+
+router.delete('/deleteUser',controller.deleteUser)
+
+module.exports = router
